3b63b4e1ca1f9c7d19cb6694f5559856b34a068e
ram4ik <ram4ik@hotmail.com>
2023-10-19 07:49:57 -0700

    sorting with keypaths

--- KeypathsInSwiftUI/ContentView.swift
+++ KeypathsInSwiftUI/ContentView.swift
@@ -7,34 +7,62 @@
 
 import SwiftUI
 
-struct MyDataModel {
+struct MyDataModel: Identifiable {
     let id = UUID().uuidString
-    let title: MovieTitle
+    let title: String
     let count: Int
     let date: Date
 }
 
-struct MovieTitle {
-    let primary: String
-    let secondary: String
-}
+//struct MovieTitle {
+//    let primary: String
+//    let secondary: String
+//}
 
 struct ContentView: View {
     
     //@Environment(\.dismiss) var dismiss
     //@AppStorage("user_count") var userCount: Int = 0
     
-    @State private var screenTitle: String = ""
+    //@State private var screenTitle: String = ""
+    
+    @State private var dataArray: [MyDataModel] = []
     
     var body: some View {
-        Text("Key paths")
+        List {
+            ForEach(dataArray) { item in
+                VStack(alignment: .leading) {
+                    Text(item.id)
+                    Text(item.title)
+                    Text("\(item.count)")
+                    Text(item.date.description)
+                }
+                .font(.headline)
+            }
+        }
             .onAppear() {
-                let item = MyDataModel(title: MovieTitle(primary: "Alpha", secondary: "Beta"), count: 1, date: .now)
+                let array = [
+                    MyDataModel(title: "One", count: 1, date: .now),
+                    MyDataModel(title: "Two", count: 2, date: .distantFuture),
+                    MyDataModel(title: "Three", count: 3, date: .distantPast),
+                ]
                 
-                let title = item.title.primary
-                let title2 = item[keyPath: \.title.primary]
+                //let newArray = array.sorted { item1, item2 in
+                //    return item1.count < item2.count
+                //}
                 
-                screenTitle = title2
+                let newArray = array.sorted { item1, item2 in
+                    return item1[keyPath: \.count] < item2[keyPath: \.count]
+                }
+                
+                dataArray = array
+                
+                //let item = MyDataModel(title: "One", count: 1, date: .now)
+                
+                //let title = item.title
+                //let title2 = item[keyPath: \.title]
+                
+                //screenTitle = title2
             }
     }
 }
