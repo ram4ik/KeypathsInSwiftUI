799b2fdba88e4c5c7388ee3594087ebe3e8693ee
ram4ik <ram4ik@hotmail.com>
2023-10-19 07:29:06 -0700

    Initial Commit

---
+++ KeypathsInSwiftUI/KeypathsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeypathsInSwiftUIApp.swift
+//  KeypathsInSwiftUI
+//
+//  Created by admin on 10/19/23.
+//
+
+import SwiftUI
+
+@main
+struct KeypathsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
