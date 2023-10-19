e01441d533a1914477698db514ebc36240d4b9b6
ram4ik <ram4ik@hotmail.com>
2023-10-19 07:37:34 -0700

    nested keypaths

--- KeypathsInSwiftUI/ContentView.swift
+++ KeypathsInSwiftUI/ContentView.swift
@@ -7,15 +7,35 @@
 
 import SwiftUI
 
+struct MyDataModel {
+    let id = UUID().uuidString
+    let title: MovieTitle
+    let count: Int
+    let date: Date
+}
+
+struct MovieTitle {
+    let primary: String
+    let secondary: String
+}
+
 struct ContentView: View {
+    
+    //@Environment(\.dismiss) var dismiss
+    //@AppStorage("user_count") var userCount: Int = 0
+    
+    @State private var screenTitle: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Key paths")
+            .onAppear() {
+                let item = MyDataModel(title: MovieTitle(primary: "Alpha", secondary: "Beta"), count: 1, date: .now)
+                
+                let title = item.title.primary
+                let title2 = item[keyPath: \.title.primary]
+                
+                screenTitle = title2
+            }
     }
 }
 
